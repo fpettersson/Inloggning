253f268640d02193a350eea46dbf169fa442b48f
filip pettersson <fpettersson11@hotmail.com>
2017-06-08 10:00:40 +0200

    Databas info

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_hostname = 'localhost'; // variabel för hostname
+$db_database = 'db';  // variabel för databasnamnet
+$db_username = 'filip'; // variabel för användarnamnet till databas
+$db_password = '123456'; // variabel för lösenord till databasen
+
+?>
