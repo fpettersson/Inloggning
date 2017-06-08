4a313f0b00249eaba84b70853c943ccb96ba79b8
filip pettersson <fpettersson11@hotmail.com>
2017-06-08 10:02:56 +0200

    Inloggningssida med formulär

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br><br><br><br>
+
+</div>
+<div class="container">
+<form class="form-horizontal" action="login.php" method="post">
+<div class="form-group">
+<h1>Log in</h1>
+
+	<label for="Username">Username</label>
+	<input type="text" class="form-control" name="username" placeholder="Enter username" required><br>
+	<label for="Password">Password</label>
+	<input type="password" class="form-control" name="password" placeholder="Enter password" required><br><br>
+	<input type="submit" class="btn btn-default" value="LOGIN">
+</div>
+</form>
+</div>
+
+</div>
+</body>
+</html>
