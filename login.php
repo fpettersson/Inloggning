187ea885e4b36e270bdb08592b32608e67f9a1d5
filip pettersson <fpettersson11@hotmail.com>
2017-06-09 11:14:50 +0200

    login script

--- login.php
+++ login.php
@@ -9,34 +9,57 @@ include 'db.php';
 $connect = new mysqli($db_hostname, $db_username, $db_password, $db_database);
 if ($connect->connect_error) die($connect->connect_error);
 
-// använder filter_input för att filtrera bort specialtecken som kan användas vid sql injections
+// använder mysqli_real_escape_string för att filtrera bort specialtecken som kan användas vid sql injections
 // hämtar 'username' och 'password' som användaren skrivit in i formuläret i index.php vilket är namnet på fälten
-$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+$uid = mysqli_real_escape_string($connect, $_POST['username']);
+$pwd = mysqli_real_escape_string($connect, $_POST['password']);
 
-// query som hämtar data där användarnamnet är det som användaren skrev in
-$sql = "SELECT * FROM users WHERE name ='$username'";
+// query som hämtar data där användarnamnet är det som användaren skrev in, förbereder query
+$stmt = $connect->prepare("SELECT * FROM users WHERE name=?");
+
+// binder ? till username som ska vara en sträng (s)
+$stmt->bind_param("s", $username);
+
+$username = $uid;
+$password = $pwd;
 // kör query
-$result = mysqli_query($connect, $sql);
+$stmt->execute();
+
+// hämtar resultatet
+$result = $stmt->get_result();
+
 // sparar resultatet i $row
 $row = mysqli_fetch_assoc($result);
+
 // lägger lösenordet från raden i $pwdhash
 $pwdhash = $row['password'];
-// kollar om det lösenord som användaren skrivit in matchar med det som finns i databasen
+
+// jämför det lösenord som användaren skrivit in och kollar om det matchar med det som finns i databasen
 $hash = password_verify($password, $pwdhash);
 
-//matchar det inte med det i databasen så printas det ut en text som berättar för användaren att det angivna lösenordet/användarnamnet inte är korrekt
+// matchar det inte med det i databasen så printas det ut en text som berättar för användaren att det angivna lösenordet/användarnamnet inte är korrekt
 if ($hash == 0) {
 	echo "Användarnamnet och lösenordet matchar inte!";
 } else {
 
 // php query som kollar om användarnamnet och lösenordet matchar med det i databasen 
-$query = "SELECT * FROM users WHERE name = '$username' AND password = '$pwdhash'";
-$result = mysqli_query($connect, $query);
+$stmt = $connect->prepare("SELECT * FROM users WHERE name = ? AND password = ?");
+$stmt->bind_param("ss", $username, $password);
 
- // om uppgifterna matchar skapas en session variabel som håller reda på vem som är inloggad och man skickas vidare till 'Home'-sidan
+$username = $uid;
+$password = $pwdhash;
+$stmt->execute();
+
+$result = $stmt->get_result();
+	
+	if (!$row = mysqli_fetch_assoc($result)) {
+		echo "Användarnamnet och lösenordet matchar inte!";
+	} else {
+		// om uppgifterna matchar skapas en session variabel som håller reda på vem som är inloggad och man skickas vidare till 'Home'-sidan
 	$_SESSION['namn'] = $row['name'];
 	header("Location: Home.php");
 	}
+
+}
 
 ?>	
